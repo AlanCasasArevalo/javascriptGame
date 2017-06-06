f6fede6c0450d4e2b9aaadd47c15251eaaf6d134
Alan Casas <alancasas@MacBook-Pro-de-Alan.local>
2017-06-06 22:10:34 +0200

    Primeras lineas del juego, funcion de creacion de elementos de HTML y funcion de wrapper de los elementos html creados

--- display.js
+++ display.js
@@ -1,5 +1,16 @@
-function DOMDisplay() {
-    
+function createElement(type, className) {
+	let element = document.createElement(type);
+	if (className !== undefined){
+		element.className = className;
+		return element;
+	}
+}
+
+function DOMDisplay(parent, level) {
+	this.wrap = parent.appendChild(createElement('div', 'game'));
+	this.level = level;
+
+	//this.wrap.appendChild();
 }
 
 
